1dd882953641c4bbabf259ca14289dd03a3fca8d
Roger Freixes <rogercat23@gmail.com>
2016-02-25 17:52:52 +0100

    Pas - 39
    Slider Carousel amb la pàgina principal esta tot programat i funciona
    pefecció que he canviat estil amb colors i etc. Només falta tenir idees
    que posar les fotos com productes amb ofertes algo aixi...

--- index.php
+++ index.php
@@ -26,21 +26,50 @@
                 </ul>
             </div>
             <div id="qd_cos">
-				<div id="myCarousel" class="carousel slide">
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicadors -->
 					<ol class="carousel-indicators">
 						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
 						<li data-target="#myCarousel" data-slide-to="1"></li>
 						<li data-target="#myCarousel" data-slide-to="2"></li>
 					</ol>
-					<!-- Carousel items -->
+					<!-- Wrapper for sliders for items -->
 					<div class="carousel-inner">
-						<div class="active item">…</div>
-						<div class="item">…</div>
-						<div class="item">…</div>
+						<div class="item active">
+							<img src="img\Carousel\Prova1.jpg" alt="Prova">
+							<div class="carousel-caption">
+								<h1>Prova</h1>
+								<p>
+									Aqui hi ha oferta bona ....
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img src="img\Carousel\Prova2.jpg" alt="Prova2">
+							<div class="carousel-caption">
+								<h1>Prova2</h1>
+								<p>
+									Aqui hi ha oferta bona ....
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img src="img\Carousel\Prova3.jpg" alt="Prova3">
+							<div class="carousel-caption">
+								<h1>Prova3</h1>
+								<p>
+									Aqui hi ha oferta bona ....
+								</p>
+							</div>
+						</div>
 					</div>
-					<!-- Carousel nav -->
-					<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-					<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+					<!-- Carousel nav (controls left and right)-->
+					<a class="carousel-control left" href="#myCarousel" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left color-default" aria-hidden="true"></span>
+					</a>
+					<a class="carousel-control right" href="#myCarousel" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right color-default" aria-hidden="true"></span>
+					</a>
 				</div>     
             </div>
         </div>
